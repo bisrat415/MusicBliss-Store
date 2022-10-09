0df6192bbb6f7b8b9fb4816badc682091922cfbb
bk415 <bisratgirma943@gmail.com>
2022-10-09 00:11:26 -0400

    minor changes

--- dynamic.js
+++ dynamic.js
@@ -1,6 +1,6 @@
 const context = {
-    title: 'Welcome to Musicon',
-    body: 'Musicon is a budding musical storefront with a mission to share the joy of music. These magnificent auditory tools are designed with musical creators, like you, in mind. Hobbyists, beginners, and experts alike can appreciate the resplendent sounds supplied by each and every instrument we carry. Join us in delivering the euphoric vibrations of melodia to the citizens of the world!',
+    title: 'Welcome to MusicBliss',
+    body: 'MusicBliss is a budding musical storefront with a mission to share the joy of music. These magnificent auditory tools are designed with musical creators, like you, in mind. Hobbyists, beginners, and experts alike can appreciate the resplendent sounds supplied by each and every instrument we carry. Join us in delivering the euphoric vibrations of melodia to the citizens of the world!',
     instruments: [
       {
         image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/electronic-keyboard.png',
